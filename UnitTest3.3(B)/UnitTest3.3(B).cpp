a57eac36b42812bd53856dc755394f92fe9e386d
ElitLien <supervlad586@gmail.com>
2021-05-03 20:54:07 +0300

    first master commit

---
+++ UnitTest3.3(B)/UnitTest3.3(B).cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_3.3(B)/Vector2D.h"
+#include "../Lab_3.3(B)/Vector2D.cpp"
+#include "../Lab_3.3(B)/Pair.h"
+#include "../Lab_3.3(B)/Pair.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Vector2D a(3, 4);
+			Assert::AreEqual(a.Modul(), 5.0);
+		}
+	};
+}
+
